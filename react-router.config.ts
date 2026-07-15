c52410e813340ebff2b9339f5cae15756ff7fd24
aliyun5144629979 <510129963@qq.com>
2026-07-16 00:19:53 +0800

    Use vercelPreset for proper Vercel deployment

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,8 @@
 import type {Config} from '@react-router/dev/config';
 import {hydrogenPreset} from '@shopify/hydrogen/react-router-preset';
+import {vercelPreset} from '@vercel/react-router';
 
 export default {
   ssr: true,
-  presets: [hydrogenPreset()],
+  presets: [hydrogenPreset(), vercelPreset()],
 } satisfies Config;
